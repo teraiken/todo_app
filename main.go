e9533efa5c3b4afd3402c10bbf14211e8b77a390
teraiken <k_terai@aivick.co.jp>
2023-08-08 14:46:29 +0900

    user get

--- main.go
+++ main.go
@@ -17,11 +17,17 @@ func main() {
 
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.Password = "testtest"
-	fmt.Println(u)
+	/*
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.Password = "testtest"
+		fmt.Println(u)
 
-	u.CreateUser()
+		u.CreateUser()
+	*/
+
+	u, _ := models.GetUser(1)
+
+	fmt.Println(u)
 }
