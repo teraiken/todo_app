a4b9c62207d3b09037e8e04f24670bd45ddb698a
teraiken <k_terai@aivick.co.jp>
2023-08-09 15:37:32 +0900

    todos get by user

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 
 	/*
 		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
+		u.Name = "test2"
+		u.Email = "test2@example.com"
 		u.Password = "testtest"
 		fmt.Println(u)
 
@@ -44,10 +44,20 @@ func main() {
 		fmt.Println(t)
 	*/
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("Second Todo")
+	/*
+		user, _ := models.GetUser(3)
+		user.CreateTodo("Third Todo")
+	*/
 
-	todos, _ := models.GetTodos()
+	/*
+		todos, _ := models.GetTodos()
+		for _, v := range todos {
+			fmt.Println(v)
+		}
+	*/
+
+	user3, _ := models.GetUser(3)
+	todos, _ := user3.GetTodosByUser()
 	for _, v := range todos {
 		fmt.Println(v)
 	}
